e1a106ea91124f91cbea12af9d0d6709cb9e47a3
brunatb <brutopanb@gmail.com>
2021-02-01 20:28:43 -0300

    users table

---
+++ src/models/User.js
@@ -0,0 +1,39 @@
+const { Sequelize } = require('sequelize');
+const sequelize = require('../utils/database');
+
+class User extends Sequelize.Model {}
+
+User.init(
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    password: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      type: Sequelize.DATE,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'user',
+  },
+);
+
+module.exports = User;
