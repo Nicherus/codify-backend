4dc733ecb8065703f4bc466c64e8c5c34339497e
brunatb <brutopanb@gmail.com>
2021-02-03 18:11:16 -0300

    changed routers dinamics

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const cors = require('cors');
 require('express-async-errors');
 const clientsRouter = require('./routers/clientsRouter');
-const coursesRouter = require('./routers/coursesRouter');
 const ConflictError = require('./errors/ConflictError');
 const InexistingId = require('./errors/InexistingId');
 
@@ -13,12 +12,11 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/clients', clientsRouter);
-app.use('/courses', coursesRouter);
 
 /* eslint-disable-next-line no-unused-vars */
 app.use((error, req, res, next) => {
   if (error instanceof ConflictError) return res.status(409).send({ error: 'Conflito de dados.' });
-  if( error instanceof InexistingId) return res.status(403).send({error: 'Id does not exists'})
+  if (error instanceof InexistingId) return res.status(403).send({ error: 'Id does not exists' });
   return res.status(500).json(error);
 });
 
--- src/routers/clientsRouter.js
+++ src/routers/clientsRouter.js
@@ -2,20 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-const signupSchema = require('../schemas/signupSchema');
-const usersController = require('../controllers/usersController');
+const usersRouter = require('./usersRouter');
+const coursesRouter = require('./coursesRouter');
 
-router.post('/signup', async (req, res) => {
-  const signupValidation = signupSchema.validate(req.body).error;
-  if (signupValidation) {
-    return res.status(422).send({
-      error: signupValidation.message === 'Senhas diferentes.'
-        ? signupValidation.message
-        : 'Verifique seus dados.',
-    });
-  }
-  await usersController.postSignup(req.body);
-  return res.sendStatus(201);
-});
+router.use('/users', usersRouter);
+router.use('/courses', coursesRouter);
 
 module.exports = router;
