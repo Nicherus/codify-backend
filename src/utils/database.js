9422448f0d48a12fca6c8b0bf724d47ca86a3a74
luguin444 <gustavo.ganimi@engenharia.ufjf.br>
2021-02-05 13:38:29 -0300

    fix: deploy settings

--- src/utils/database.js
+++ src/utils/database.js
@@ -6,6 +6,10 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   ssl: {
     require: false,
     rejectUnauthorized: false,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+    }
   },
 });
 
