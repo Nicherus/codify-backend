a05e3c0077f9d8cb722fe7ca6dbaca6962cca9b4
Nicherus <joaonichev@gmail.com>
2021-02-05 13:59:07 -0300

    deploy

--- src/utils/database.js
+++ src/utils/database.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   logging: false,
   dialectOptions: {
       ssl: {
-        rejectUnauthorized: false,
+        rejectUnauthorized: true,
     },
   },
 });
