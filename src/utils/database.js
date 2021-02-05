7468b05d9d1acbcb4d9bba92984ef2ea3f815e76
luguin444 <gustavo.ganimi@engenharia.ufjf.br>
2021-02-05 13:42:02 -0300

    fix: deploy settings

--- src/utils/database.js
+++ src/utils/database.js
@@ -3,13 +3,11 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  ssl: {
-    require: false,
-    rejectUnauthorized: false,
-    dialectOptions: {
+  logging: false,
+  dialectOptions: {
       ssl: {
-        rejectUnauthorized: false
-    }
+        rejectUnauthorized: false,
+    },
   },
 });
 
